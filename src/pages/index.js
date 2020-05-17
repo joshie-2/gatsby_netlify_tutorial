212e17204dbba360aeedf9474362e6a5160c3054
Joshua Selfe <josh@modjob.io>
2020-05-17 11:28:30 -0400

    inital commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,17 @@
 import React from "react"
-
-export default () => <div>Hello world!</div>
+import { Link } from "gatsby"
+import Header from "../components/header"
+export default function Home() {
+  return (
+    <div style={{ color: `purple` }}>
+      <Link to="/contact/">Contact</Link>
+      <Header headerText="Hello Gatsby" />
+      <p>What a world</p>
+      <img
+        src="https://images.unsplash.com/photo-1544890225-2f3faec4cd60?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60"
+        alt=""
+        srcset=""
+      />
+    </div>
+  )
+}
