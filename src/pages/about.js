a7736e74b5588a528fe61067f8ff543d742c5527
Joshua Selfe <joshua.selfe@gmail.com>
2020-05-17 20:23:42 -0400

    added navbar compnent & updated header prop

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
 import React from "react"
+import NavBar from "../components/navBar"
 import Header from "../components/header"
 export default function About() {
   return (
     <div style={{ color: `purple` }}>
-      <Header headerText="About Gatsby" />
+      <NavBar />
+      <Header headerText="About Page" />
       <p>this is my place holder for my about page</p>
     </div>
   )
