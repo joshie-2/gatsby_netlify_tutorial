8f0ddb2f2ad9c1cc10019a7319acf98de3f41ed4
Joshua Selfe <joshua.selfe@gmail.com>
2020-05-17 20:11:19 -0400

    changed style to purple

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,7 @@ import React from "react"
 import Header from "../components/header"
 export default function About() {
   return (
-    <div style={{ color: `teal` }}>
+    <div style={{ color: `purple` }}>
       <Header headerText="About Gatsby" />
       <p>this is my place holder for my about page</p>
     </div>
