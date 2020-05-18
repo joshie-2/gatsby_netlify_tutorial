4adf4db61ad604adf6a806423e51f0ed0d65412f
Joshua Selfe <joshua.selfe@gmail.com>
2020-05-17 20:10:44 -0400

    removed image

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,11 +7,6 @@ export default function Home() {
       <Link to="/contact/">Contact</Link>
       <Header headerText="Hello Gatsby" />
       <p>What a world</p>
-      <img
-        src="https://images.unsplash.com/photo-1544890225-2f3faec4cd60?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60"
-        alt=""
-        srcset=""
-      />
     </div>
   )
 }
