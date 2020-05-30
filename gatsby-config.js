45752340f25111d0f2b9c76213d7c3fc6bc9b513
Joshua Selfe <joshua.selfe@gmail.com>
2020-05-30 11:21:56 -0400

    added linkedin & twitter, config env variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,20 @@
+const activeEnv =
+  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
+console.log(`Using environment config: '${activeEnv}'`)
+require("dotenv").config({
+  path: `.env.${activeEnv}`,
+})
+
+require("dotenv").config({
+  path: `.env.${activeEnv}`,
+})
+
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: "My Personal Site",
+    author: "Joshua Selfe",
+    linkedInPofileUrl: "https://www.linkedin.com/in/joshuaselfe/",
+    twitterProfileUrl: "https://twitter.com/joshua_selfe",
+  },
   plugins: ["gatsby-plugin-sass"],
 }
