0ce9093c75f082e29932fcabd7c0a0ba6a94f88f
Joshua Selfe <joshua.selfe@gmail.com>
2020-05-30 12:32:08 -0400

    create query to list filesystem posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,9 +1,37 @@
 import React from "react"
 import Layout from "../components/layout"
+import { graphql, useStaticQuery } from "gatsby"
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              author
+              date
+            }
+            timeToRead
+          }
+        }
+      }
+    }
+  `)
   return (
     <Layout>
-      <p>This is my blog page</p>
+      <h1>Blog</h1>
+      <ol>
+        {data.allMarkdownRemark.edges.map(post => {
+          return (
+            <li>
+              <h2>{post.node.frontmatter.title}</h2>
+              <p>{post.node.frontmatter.date}</p>
+              <p>time to read: {post.node.timeToRead} min</p>
+            </li>
+          )
+        })}
+      </ol>
     </Layout>
   )
 }
