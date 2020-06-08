11bae4c9345653044d16d514e93fc9a95652776e
Joshua Selfe <joshua.selfe@gmail.com>
2020-06-07 21:05:33 -0400

    config react helmet & emoji accessability

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,30 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
+import Head from "../components/head"
 
 const NotFound = () => {
   return (
     <Layout>
-      <h1>Ops! 😫😫 Not a Page on My Site </h1>
+      <Head title="Not Found 404" />
+      <h1>
+        Ops!
+        <span role="img" aria-label="mad face">
+          😫
+        </span>
+        <span role="img" aria-label="mad face">
+          😫
+        </span>{" "}
+        Not a Page on My Site{" "}
+      </h1>
       <h3>
-        👉 <Link to="/">click here to go home</Link> 👈
+        <span role="img" aria-label="hand pointing right">
+          👉
+        </span>
+        <Link to="/">click here to go home</Link>
+        <span role="img" aria-label="hand pointing left">
+          👈
+        </span>
       </h3>
     </Layout>
   )
